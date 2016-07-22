57fa9d2fd2c5b96c756aead52b6cb80cf9d95796
Connor Howells <chowells@sae.au>
2016-07-22 14:19:17 +1000

    Initial Commit

---
+++ Assets/Scripts/CharacterScript/Player2DController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2DController : MonoBehaviour {
+
+    public float Speed;
+    
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //character Movement Controls
+        transform.Translate(new Vector2(0, 1) * Speed * Input.GetAxis("Vertical"), Space.World);
+        transform.Translate(new Vector2(1, 0) * Speed * Input.GetAxis("Horizontal"), Space.World);
+
+        Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    }
+}
---
+++ Assets/Scripts/Enemy Script/EnemyAIScript00.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAIScript00 : MonoBehaviour {
+
+	public Transform player;
+	public float playerDistance;
+	public float rotationDamping;
+	public float moveSpeed;
+    public GameObject playerObject;
+    public GameObject enemy;
+    public float spawntime = 10f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("spawn", spawntime, 0);
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        {
+            playerDistance = Vector3.Distance(player.position, transform.position);
+            if (playerDistance < 100f)
+            {
+                chase();
+                lookAtPlayer();
+            }
+        }
+    }
+	
+
+	void lookAtPlayer()
+	{
+		Quaternion rotation = Quaternion.LookRotation (player.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationDamping);
+	}
+
+	void chase()
+	{
+		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+    
+    void spawn()
+    {
+        Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0);
+        Instantiate(enemy, position, Quaternion.identity);      
+    }
+}
+	
+
