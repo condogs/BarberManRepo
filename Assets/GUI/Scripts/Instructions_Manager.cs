cd89174900632b9f180a544a06f9a562e8b7ef88
Anurag Gulati <agulati@sae.au>
2016-07-29 15:37:26 +1000

    Instructions screen finished, options screen finished. Just need the audio tracks from the audio guy.

---
+++ Assets/GUI/Scripts/Instructions_Manager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Instructions_Manager : MonoBehaviour {
+
+	public GameObject textObjectives;
+	public GameObject textPlayerControls;
+	public GameObject textEnemies;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ObjectivesClicked ()
+	{
+		textObjectives.SetActive (true);
+		textPlayerControls.SetActive (false);
+		textEnemies.SetActive (false);
+	}
+
+	public void PlayerControlsClicked ()
+	{
+		textObjectives.SetActive (false);
+		textPlayerControls.SetActive (true);
+		textEnemies.SetActive (false);
+	}
+
+	public void EnemiesClicked ()
+	{
+		textObjectives.SetActive (false);
+		textPlayerControls.SetActive (false);
+		textEnemies.SetActive (true);
+	}
+}
---
+++ Assets/GUI/Scripts/Options_Manager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Options_Manager : MonoBehaviour {
+
+	public GameObject audioOn;
+	public GameObject audioOff;
+
+	public bool audio = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		audioOn.SetActive (true);
+		audioOff.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (audio) 
+		{
+			audioOff.SetActive (false);
+			audioOn.SetActive (true);
+		}
+
+		if (!audio) 
+		{
+			audioOff.SetActive (true);
+			audioOn.SetActive (false);
+		}
+	}
+
+	public void AudioPressed ()
+	{
+		audio = !audio;
+	}
+}
