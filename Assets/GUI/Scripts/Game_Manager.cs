c2bb100a6caa2df674fa4b510760b33028c78fdc
Anurag Gulati <Anurag Gulati>
2016-07-29 21:23:12 +1000

    Pause menu semi completed. Options menu completed.

---
+++ Assets/GUI/Scripts/Game_Manager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Manager : MonoBehaviour {
+
+    public GameObject pauseMenu;
+
+    // Use this for initialization
+	void Start ()
+    {
+        pauseMenu.SetActive (false);	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseClicked();
+        }
+	}
+
+    public void PauseClicked()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive (true);
+    }
+
+    public void ResumeClicked()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void RestartClicked()
+    {
+        // Input code
+    }
+
+    public void MenuClicked()
+    {
+        // Input code
+    }
+}
