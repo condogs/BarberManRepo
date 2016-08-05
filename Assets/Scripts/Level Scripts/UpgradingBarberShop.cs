a3dea1d04c4bbb8cab94fb230cb47c865e4046a7
Connor Howells <chowells@sae.au>
2016-08-05 15:34:05 +1000

    Lots of refinement

--- Assets/Scripts/Level Scripts/UpgradingBarberShop.cs
+++ Assets/Scripts/Level Scripts/UpgradingBarberShop.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class UpgradingBarberShop : MonoBehaviour {
+
+
+
+
+
 
 	// Use this for initialization
 	void Start () {
 	
+
+
+
+
+
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+
+
+
+
+
 	}
 }
