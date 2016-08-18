d2bc2ac8aec0e587af537160b5e5e7adb74545fc
Anurag Gulati <Anurag Gulati>
2016-08-19 00:57:02 +1000

    Completed game over scene UI. Code needs to be implemented.

---
+++ Assets/UI/GUI/Scripts/Scene_Manager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void RestartPressed()
+    {
+
+    }
+}
